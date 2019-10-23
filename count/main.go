0845c665607d7f7ef18f1e918c258d534446b6a3
oiar <snowoiar@gmail.com>
2019-10-23 23:50:11 +0800

    increase

--- count/main.go
+++ count/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"log"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const hobby string = "999"
+const hobby string = "999999"
 
 func main() {
 	hang := make(chan struct{})
@@ -16,7 +18,23 @@ func main() {
 	slaveOne := CreateCon("3307")
 	slaveTwo := CreateCon("3308")
 
+	master.CreateDB()
+	master.CreateTable()
+
+	slaveOne.CreateDB()
+	slaveOne.CreateTable()
+
+	slaveTwo.CreateDB()
+	slaveTwo.CreateTable()
+
 	start := time.Now()
+	for i := 0; i < 1000000; i++ {
+		master.InsertData(hobby, strconv.Itoa(i))
+		log.Println("[current count]", i)
+		log.Println("[current insert time]", time.Now().Sub(start).Seconds())
+	}
+
+	start = time.Now()
 	go func() {
 		var code int
 		for code != 1 {
@@ -50,6 +68,7 @@ type DB struct {
 	*sql.DB
 }
 
+// CreateCon create a db conn to local with given port
 func CreateCon(port string) *DB {
 	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:"+port+")/?parseTime=true")
 	if err != nil {
@@ -58,6 +77,38 @@ func CreateCon(port string) *DB {
 
 	return &DB{
 		db,
+	}
+}
+
+func (db *DB) CreateDB() error {
+	result, err := db.Exec("CREATE DATABASE IF NOT EXISTS masterSlaveDB;")
+	if affected, _ := result.RowsAffected(); affected == 0 {
+		return errors.New("[create order] : create order affected 0 rows")
+	}
+	if err != nil {
+		log.Println(err)
+	}
+
+	return nil
+}
+
+func (db *DB) CreateTable() {
+	_, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS masterSlaveDB.masterSlaveTable (" +
+			"id bigint(20) unsigned NOT NULL AUTO_INCREMENT, " +
+			"name varchar(50) DEFAULT NULL, " +
+			"hobbies varchar(200) DEFAULT NULL, " +
+			"PRIMARY KEY (id)" +
+			") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; ")
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (db *DB) InsertData(name, hobbies string) {
+	_, err := db.Exec("INSERT INTO masterSlaveDB.masterSlaveTable (name, hobbies) VALUES (?,?)", name, hobbies)
+	if err != nil {
+		log.Println(err)
 	}
 }
 

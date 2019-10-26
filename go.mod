0ad0bcda5a63f4aaa26ee6180008f61422b390e3
oiar <snowoiar@gmail.com>
2019-10-26 18:10:16 +0800

    xorm

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	github.com/go-gorp/gorp v2.0.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.4.1
+	github.com/go-xorm/xorm v0.7.9
 	github.com/mattn/go-sqlite3 v1.11.0
 	gopkg.in/gorp.v1 v1.7.2
+	xorm.io/xorm v0.8.0 // indirect
 )
---
+++ orm/xorm/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+)
+
+func main() {
+	engine, err := xorm.NewEngine("mysql", "root:123456@tcp(127.0.0.1:3306)/masterSlaveDB?parseTime=true")
+	if err != nil {
+		log.Println(err, "err")
+	}
+
+	err = engine.Sync2(new(User))
+	if err != nil {
+		log.Println(err, "[CreateTable err]")
+	}
+
+	var user User
+	user.Name = "aaa"
+	user.Address = "North"
+	affected, err := engine.Insert(user)
+	if err != nil {
+		log.Println(err, "[insert err]")
+	}
+	log.Println(affected, "[insert affected]")
+
+	result, err := engine.Id(2).Get(&user)
+	if err != nil {
+		log.Println(err, "[get err]")
+	}
+	log.Println(result, "[get result]")
+
+	// affected, err = engine.Id(1).Delete(user)
+	// if err != nil {
+	// 	log.Println(err, "[delete err]")
+	// }
+	// log.Println(affected, "[delete affected]")
+}
+
+type User struct {
+	Id int64
+	// `xorm:"INT(11) NOT NULL AUTO_INCREMENT 'id'"`
+	Name    string `xorm:"VARCHAR(64) 'name'"`
+	Address string `xorm:"VARCHAR(256) 'address'"`
+}
